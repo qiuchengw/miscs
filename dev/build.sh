27994c74451450e4a93599666d52f00d8b379428
qiuchengw <qiuchengw@163.com>
2022-08-09 23:42:53 +0800

    调整编译顺序，支持systemctl

--- dev/build.sh
+++ dev/build.sh
@@ -1,20 +1,12 @@
 # 配置GDB调试显示的漂亮点
 G_SRC_FILE=$1
 
-echo "installing depends ..."
-yum update -y 
-# gettext： 在编译git的时候需要执行msgfmt命令
-# cmake：需要使用openssl-devel
-# yum install -y wget mlocate which vim zsh gettext openssl-devel
-
-echo "import keys ..."
+echo "==> install devltoolset-11!"
 wget http://mirrors.163.com/centos/RPM-GPG-KEY-CentOS-7
 rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
 
 yum install -y centos-release-scl scl-utils-build 
-echo "==> install devltoolset-11!"
 yum install -y devtoolset-11
-yum autoremove -y
 echo "==> depends installed!"
 
 # scl enable devtoolset-11 bash 
@@ -42,12 +34,16 @@ make -j$nproc NO_TCLTK=1 && make install
 echo "==> building cmake ..."
 cd ${G_SRC_FILE}/cmake-3.23.3/ 
 ./bootstrap --parallel=$nproc --prefix=/usr/local
-make -j$nproc && make install
+make -j$nproc 
+make install
 
 # install python
 echo "==> building python ..."
 cd ${G_SRC_FILE}/Python-3.8.10/
-./configure --enable-optimizations --with-ssl --prefix=/usr/local/python3.8.10
+# --enable-optimizations 和 enable-shared好像有冲突，会导致编译错误：
+# error: number of counters in profile data for function 'Py_GetBuildInfo' does not match its profile data (counter 'arcs', expected 2 and have 6) [-Werror=coverage-mismatch]
+#    35 | Py_GetBuildInfo(void)
+./configure --with-ssl --enable-shared --disable-test-modules --prefix=/usr/local/python3.8.10
 make -j$nproc && make install
 # 设置路径
 ln -s /usr/local/python3.8.10/bin/python3 /usr/local/bin/python3
@@ -56,8 +52,8 @@ ln -s /usr/local/python3.8.10/bin/pip3 /usr/local/bin/pip3
 # install boost
 echo "==> building boost ..."
 cd ${G_SRC_FILE}/boost_1_79_0/ 
-./bootstrap.sh --with-libraries=all --with-toolset=gcc 
-./b2 toolset=gcc-${GCC_VER} && ./b2 install --prefix=/usr
+# ./bootstrap.sh --with-libraries=all --with-toolset=gcc 
+# ./b2 toolset=gcc-${GCC_VER} && ./b2 install --prefix=/usr
 
 # 删除本地的安装文件，确保镜像大小较小
 cd ${G_SRC_FILE}/../
